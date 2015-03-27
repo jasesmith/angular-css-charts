5f1ae62e4b2de32b73c17393b5a163699b4c5be6
Jase Smith <jase@star29.com>
2015-03-27 13:34:12 -0600

    more segment math

--- charts/donutChartDirective.js
+++ charts/donutChartDirective.js
@@ -9,9 +9,10 @@ angular.module('app')
             },
             controller: ['$scope', function($scope){
                 var defaultConfig = {
+                    capacity: 500,
                     segments: [
-                        {label: 'thing 1', value: 230, color: colors[0].name},
-                        {label: 'thing 2', value: 1768, color: colors[1].name},
+                        {label: 'thing 1', value: 450, color: colors[0].name},
+                        {label: 'thing 2', value: 768, color: colors[1].name},
                         {label: 'thing 3', value: 100, color: colors[2].name},
                         {label: 'thing 4', value: 130, color: colors[3].name},
                         {label: 'thing 5', value: 100, color: colors[4].name},
@@ -23,58 +24,119 @@ angular.module('app')
                 // pull in app's menu config or from MenuConfig provider
                 $scope.config = $.extend(true, angular.copy(defaultConfig), $scope.config);
 
+                var setDegree = function(value, total){
+                    return (value/total * 360).toFixed(2);
+                };
+
+                var setPercent = function(value, total){
+                    return (value/total * 100).toFixed(2);
+                };
+
+                var splitSegment = function(segment, index, segments){
+                    if(segment._degrees > 180) {
+                        var newSegment = angular.copy(segment);
+                        newSegment._degrees = (segment._degrees - 180).toFixed(2) * 1;
+                        segment._degrees = 180;
+                        newSegment.label = segment.label + ' (cont)';
+                        newSegment._percent = segment._percent;
+                        newSegment._continued = true;
+                        if(index) {
+                            segments.splice(index + 1, 0, newSegment);
+                        } else {
+                            segments.push(segment, newSegment);
+                        }
+                    }
+                };
+
                 var fixSectorSizes = function(segments){
                     var l = segments.length;
                     var total = 0;
-                    _.each(segments, function(s){
-                        total = total + s.value;
-                    });
+                    var actual = 0;
+                    var overage = 0;
+                    if($scope.config.capacity) {
+                        total = $scope.config.capacity;
+                        _.each(segments, function(s){
+                            actual = actual + s.value;
+                        });
+                        if(actual > total) {
+                            overage = actual-total;
+                            total = actual;
+                        }
+                        window.console.log(total, overage);
+                    } else {
+                        _.each(segments, function(s){
+                            total = total + s.value;
+                        });
+                    }
                     _.each(segments, function(segment, i){
                         // convert segment value into nº (degrees)
-                        if(!segment.isContinued) {
-                            segment.value = (segment.value/total * 360).toFixed(2);
+                        if(!segment._continued) {
+                            segment._degrees = setDegree(segment.value, total);
+                            segment._percent = setPercent(segment.value, total);
+
                             // more than 180º causes display failure
-                            if(segment.value > 180) {
-                                var newSegment = angular.copy(segment);
-                                newSegment.value = (segment.value - 180 * 1).toFixed(2);
-                                segment.value = 180 * 1;
-                                newSegment.label = segment.label + ' (cont)';
-                                newSegment.isContinued = true;
-                                segments.splice(i+1, 0, newSegment);
-                            }
+                            splitSegment(segment, i, segments);
                         }
                     });
-                    if(l < segments.length) {
-                        segments[segments.length-1].value = (segments[segments.length-1].value/total * 360).toFixed(2);
+                    var lastSegment = _.last(segments);
+                    if(l < segments.length && !lastSegment._continued) {
+                        lastSegment._degrees = setDegree(lastSegment.value, total);
+                        lastSegment._percent = setPercent(lastSegment.value, total);
                     }
+                    if(overage) {
+                        var overageSegment = {
+                            color: 'dark'
+                        };
+                        overageSegment._degrees = setDegree(overage, total);
+                        overageSegment._percent = setPercent(overage, total);
+                        overageSegment._overage = true;
+                        splitSegment(overageSegment, false, segments);
+                        window.console.log(overageSegment);
+                    }
+                    $scope.config._overage = overage;
+                    $scope.config._total = total;
+                    $scope.config._actual = actual;
                 };
 
                 fixSectorSizes($scope.config.segments);
 
                 $scope.setSector = function(segment, index) {
                     if(segment) {
-                        var val = segment.value * 1;
+                        var d = segment._degrees * 1;
                         index = index || 0;
 
-                        // if(index > 0) {
-                            _.each($scope.config.segments, function(s, i){
-                                if(i < index) {
-                                    val = val + s.value * 1;
-                                }
-                            });
-                        // }
+                        _.each($scope.config.segments, function(s, i){
+                            if(i < index) {
+                                d = d + s._degrees * 1;
+                            }
+                        });
 
-                        return {
-                            transform: 'rotate('+val+'deg)'
+                        var styles = {
+                            transform: 'rotate('+d+'deg)'
                         };
+
+                        if(segment._overage) {
+                            if(segment._continued) {
+                                styles.transform = 'rotate('+d+'deg)';
+                            } else {
+                                styles.opacity = 0.5;
+                                styles.transform = 'rotate('+d+'deg) scale(.85)';
+                            }
+                        }
+                        return styles;
                     }
                 };
             }],
 
             template: '' +
                 '<div class="chart" type="donut">' +
-                    '<div ng-repeat="segment in config.segments" class="segment" ng-style="setSector(config.segments[$index - 1], $index-1)">' +
-                        '<div class="wedge bg-{{segment.color}}" ng-style="setSector(segment)" title="{{segment.label}}: {{segment.value}}"></div>' +
+                    '<div ng-repeat="segment in config.segments" class="segment" title="{{segment.label}}: {{segment.value}} {{segment._percent}}% {{segment._degrees}}&deg;" ng-style="setSector(config.segments[$index - 1], $index-1)">' +
+                        '<div class="wedge bg-{{segment.color}}" ng-style="setSector(segment)"></div>' +
+                    '</div>' +
+                    '<div class="center-label">' +
+                        'cap: {{config.capacity}}<br />' +
+                        'used: {{config._actual}}<br />' +
+                        'over: {{config._overage}}<br />' +
                     '</div>' +
                 '</div>' +
             '',
