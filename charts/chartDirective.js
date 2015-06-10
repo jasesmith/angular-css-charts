ce35a61077b48b66ab9fd06537fadd4ded7df4d9
Jase <jase@star29.com>
2015-06-10 17:20:39 -0600

    big restructure with smarter templates

--- charts/chartDirective.js
+++ charts/chartDirective.js
@@ -1,49 +1,29 @@
 angular.module('app')
-    .directive('chart', [function() {
+    .directive('cssChart', [function() {
+        var me = this;
 
-        var setDegree = function(value, total){
-            return (value/total * 360).toFixed(1) * 1;
-        };
-
-        var setPercent = function(value, total){
-            return (value/total * 100).toFixed(1) * 1;
-        };
-
-        var getTotals = function(segments, capacity){
-            var total = 0;
-            var actual = 0;
-            var overage = 0;
-            var available = 0;
-
-            _.each(segments, function(segment){
-                actual = actual + (segment.value * 1);
-            });
-
-            total = actual;
-
-            if(capacity) {
-                total = capacity;
-                if(actual > total) {
-                    overage = actual - total;
-                } else {
-                    available = total - actual;
-                }
+        me._setDegree = function(value, total){
+            if (total === 0) {
+                return 0;
+            } else {
+                return parseFloat(value / total * 360);
             }
-
-            return {
-                total: { num: total, pct: setPercent(total, total)},
-                actual: {num: actual, pct: setPercent(actual, total)},
-                overage: {num: overage, pct: setPercent(overage, total)},
-                available: {num: available, pct: setPercent(available, total)}
-            };
         };
 
-        var _previousSector = function(segments, index){
+        me._setPercent = function(value, total){
+            if (total === 0) {
+                return 0;
+            } else {
+                return parseFloat(value / total * 100);
+            }
+        };
+
+        me._previousSector = function(segments, index){
             return (index === 0 ? false : segments[index-1]);
         };
 
         // more than 180º causes display failure
-        var _splitSector = function(segment, split){
+        me._splitSector = function(segment, split){
             split = split || 180;
             var array = [];
             var times = Math.ceil(segment._degrees/split);
@@ -68,17 +48,47 @@ angular.module('app')
             return array;
         };
 
-        var processSectors = function(segments, capacity, calcs){
+        me.getTotals = function(segments, capacity){
+            var total = 0;
+            var actual = 0;
+            var overage = 0;
+            var available = 0;
+
+            _.each(segments, function(segment){
+                actual = actual + parseFloat(segment.value);
+            });
+
+            total = actual;
+
+            if(capacity) {
+                total = capacity;
+                if(actual > total) {
+                    overage = actual - total;
+                } else {
+                    available = total - actual;
+                }
+            }
+
+            return {
+                total: { num: total, pct: me._setPercent(total, total)},
+                actual: {num: actual, pct: me._setPercent(actual, total)},
+                overage: {num: overage, pct: me._setPercent(overage, total)},
+                available: {num: available, pct: me._setPercent(available, total)}
+            };
+        };
+
+        me.processSectors = function(format, segments, capacity, calcs){
             // store some metrics
-            calcs = calcs || getTotals(segments, capacity);
+            calcs = calcs || me.getTotals(segments, capacity);
             var prev = 0;
 
             segments = _.map(segments, function(segment){
-                segment._degrees = setDegree(segment.value, calcs.total.num);
-                segment._percent = setPercent(segment.value, calcs.total.num);
+                segment._degrees = me._setDegree(segment.value, calcs.total.num);
+                segment._percent = me._setPercent(segment.value, calcs.total.num);
+                segment._percentCap = me._setPercent(segment.value, capacity);
 
-                if(segment._degrees > 180) {
-                    return _splitSector(segment);
+                if(segment._degrees > 180 && format === 'radial') {
+                    return me._splitSector(segment);
                 } else {
                     return [segment];
                 }
@@ -87,55 +97,152 @@ angular.module('app')
             segments = _.flatten(segments);
 
             segments = _.each(segments, function(segment, i){
-                var previousSegment = _previousSector(segments, i);
+                var previousSegment = me._previousSector(segments, i);
 
                 prev = prev + (previousSegment ? (segment._continued ? previousSegment._degrees : previousSegment._degrees + (previousSegment._split ? previousSegment._split : 0)) : 0);
-                segment._degreeStart = prev.toFixed(1);
+                segment._degreeStart = parseFloat(prev);
             });
 
             return segments;
         };
 
+        me.chartTemplateUrl = function(templateUrl, chartType){
+            return templateUrl.replace('%type%', chartType);
+        };
+
+        me.chartFormat = function(type){
+            return (type === 'donut' || type === 'pie' ? 'radial' : 'block');
+        };
+
         return {
             restrict: 'E',
+
             replace: true,
+
             scope: {
                 config: '=?'
             },
-            controller: ['$scope', function($scope){
+
+            template: '<div ng-include="templateUrl" class="chart-wrapper" format="{{format}}" type="{{type}}" ng-class="{\'animate\': config.preferences.animate}"></div>',
+
+            link: function(scope){
+
                 var defaultConfig = {
                     capacity: 0,
-                    segments: []
+                    segments: [],
+                    settings: {
+                        type: 'donut',
+                        template: 'charts/template-%type%.html'
+                    },
+                    preferences: {
+                        animate: true,
+                        precision: 1
+                    }
                 };
 
-                $scope.config = $.extend(true, defaultConfig, $scope.config);
+                scope.config = $.extend(true, angular.copy(defaultConfig), scope.config);
 
-                $scope.totals = getTotals($scope.config.segments, $scope.config.capacity);
+                scope.type = scope.config.settings.type;
+                scope.template = scope.config.settings.template;
+                scope.format = me.chartFormat(scope.type);
+                scope.templateUrl = me.chartTemplateUrl(scope.template, scope.type);
 
-                $scope.config.segments = processSectors($scope.config.segments, $scope.config.capacity, $scope.totals);
+                scope.$watch('config', function(n, o) {
+                    scope.config = $.extend(true, angular.copy(defaultConfig), scope.config);
 
-                // $scope.$watch(function(){
-                //     return $scope.config;
-                //     }, function(newVal, oldVal){
-                //         // window.console.log('dir watcher', newVal, oldVal);
-                //         if(newVal !== oldVal) {
-                //             $scope.config = $.extend(true, defaultConfig, $scope.config);
-                //             $scope.totals = getTotals($scope.config.segments, $scope.config.capacity);
-                //             $scope.config.segments = processSectors($scope.config.segments, $scope.config.capacity, $scope.totals);
-                //         }
-                //     },
-                //     true
-                // );
-            }],
+                    if(n.settings.type !== o.settings.type || n.settings.template !== o.settings.template) {
+                        scope.type = n.settings.type;
+                        scope.template = n.settings.template;
+                        scope.format = me.chartFormat(scope.type);
+                        if(!scope.template.length) {
+                            scope.template = defaultConfig.settings.template;
+                        }
+                        scope.templateUrl = me.chartTemplateUrl(scope.template, scope.type);
+                    }
 
-            link: function(scope, element, attrs){
-                scope.templateUrl = 'charts/template-' + attrs.template + '.html';
-                attrs.$observe('template',function(t){
-                    scope.templateUrl = 'charts/template-' + t + '.html';
-                });
-            },
+                    var _segments = angular.copy(scope.config.segments);
 
-            template: '<div ng-include="templateUrl"></div>'
+                    scope.capacity = angular.copy(scope.config.capacity);
 
+                    scope.totals = me.getTotals(_segments, scope.capacity);
+                    scope.segments = me.processSectors(scope.format, _segments, scope.capacity, scope.totals);
+                }, true);
+
+            }
+        };
+    }])
+    .directive('cssChartObject', ['$compile', function($compile) {
+        var me = this;
+
+        me.styleMe = function(forWhat){
+            var style = [];
+
+            // manage browser vendor styles for chart pieces
+            switch(forWhat) {
+                case 'radial-segment':
+                    style.push('transform: rotate({{segment._degreeStart}}deg) scale(1)');
+                    break;
+                case 'radial-wedge':
+                    style.push('transform: rotate({{segment._degrees}}deg)');
+                    break;
+                case 'radial-label':
+                    style.push('transform: rotate(-{{(segment._degreeStart*1) + (segment._degrees*1)}}deg)');
+                    break;
+                case 'stacked-segment':
+                    style.push('flex-basis: {{segment._percent}}%');
+                    break;
+            }
+
+            return style.join(';');
+        };
+
+        return {
+            restrict: 'A',
+
+            replace: true,
+
+            link: function (scope, element) {
+
+                var template = '';
+
+                switch(scope.type) {
+                    case 'donut':
+                    case 'pie':
+                        template = '' +
+                            '<div ng-repeat="segment in segments track by $index" class="segment" data-title="{{segment.label}}" style="' + me.styleMe('radial-segment') +'">' +
+                                '<div class="wedge {{segment.class}}" ng-class="{\'continued\': segment._continued}" ng-style="{\'color\': (segment.class ? \'\' : segment.color)}" style="' + me.styleMe('radial-wedge') +'">' +
+                                    '<div class="label"><span style="' + me.styleMe('radial-label') +'">{{segment.label}} {{segment._percent | number: config.preferences.precision}}%</span></div>' +
+                                '</div>' +
+                            '</div>';
+                        break;
+
+                    case 'bars':
+                        template = '' +
+                            '<div ng-repeat="segment in segments track by $index" ng-if="!segment._continued" percent="{{segment._percentCap}}" class="segment" data-title="{{segment.label}}" style="height: {{segment._percentCap}}%">' +
+                                '<div class="value {{segment.class}}" ng-style="{\'color\': (segment.class ? \'\' : segment.color)}">' +
+                                    '<div class="label"><span>{{segment.label}}: {{segment._percentCap | number: config.preferences.precision}}%</span></div>' +
+                                '</div>' +
+                            '</div>';
+                        break;
+
+                    case 'stacked':
+                        template = '' +
+                            '<div ng-repeat="segment in segments track by $index" class="segment" ng-if="!segment._continued && segment.value > 0" percent="{{segment._percent}}" data-title="{{segment.label}}" style="' + me.styleMe('stacked-segment') +'">' +
+                                '<div class="value {{segment.class}}" ng-style="{\'color\': (segment.class ? \'\' : segment.color)}">' +
+                                    '<div class="label"><span>{{segment.label}}: {{segment._percent | number: config.preferences.precision}}%</span></div>' +
+                                '</div>' +
+                            '</div>';
+
+                        break;
+                }
+
+                var renderTemplate = function(element, template){
+                    element.append(template);
+                    var compiled = $compile(element.contents());
+                    compiled(scope);
+                };
+
+                renderTemplate(element, template);
+            }
         };
     }]);
--- js/app.js
+++ js/app.js
@@ -3,20 +3,64 @@ angular.module('app', ['jamfu'])
 
         $scope.headline = 'CSS Charts';
         $scope.icon = 'pie-chart';
-        $scope.template = 'donut';
         $scope.spacecat = false;
+        $scope.newSegment = {};
+
+        $scope.sizeUnits = [
+            'vmin',
+            'vmax',
+            'vw',
+            'vh',
+            'rem',
+            'em',
+            'px',
+            'pt'
+        ];
+
+        $scope.unit = 'vmin';
+
         $scope.chart1 = {
+            // settings: {
+            //     type: 'donut',
+            //     template: 'examples/template-%type%.html'
+            // },
             capacity: 1850,
             segments: [
-                {label: 'apple', value: 210, color: '#fc1770'},
-                {label: 'tangerine', value: 158, color: '#ff7f36'},
-                {label: 'banana', value: 50, color: '#fff261'},
-                {label: 'kiwi', value: 180, color: '#94ca3d'},
-                {label: 'sky', value: 251, color: '#15c5ec'},
-                {label: 'berry', value: 307, color: '#c657af'},
-                {label: 'plum', value: 555, color: '#7f3fa6'},
-                // {label: 'dark', value: 655, color: '#232629'},
+                {label: 'apple', value: 210, class: 'fg-apple', xcolor: '#fc1770'},
+                {label: 'tangerine', value: 158, class: 'fg-tangerine', xcolor: '#ff7f36'},
+                {label: 'banana', value: 50, class: 'fg-banana', xcolor: '#fff261'},
+                {label: 'kiwi', value: 180, class: 'fg-kiwi', xcolor: '#94ca3d'},
+                {label: 'sky', value: 251, class: 'fg-sky', xcolor: '#15c5ec'},
+                {label: 'berry', value: 307, class: 'fg-berry', xcolor: '#c657af'},
+                {label: 'plum', value: 555, class: 'fg-plum', xcolor: '#7f3fa6'}
             ]
+        };
+
+        var initialCapacity = angular.copy($scope.chart1.capacity);
+        var initialSegments = angular.copy($scope.chart1.segments);
+
+        $scope.autoCapacity = false;
+        $scope.chartMax = initialCapacity;
+        $scope.chartSize = 35;
+
+        $scope.$watch('autoCapacity', function(n){
+            $scope.chart1.capacity = n ? ($scope.chart1.settings.type === 'bars' ? 100 : 0) : initialCapacity;
+            $scope.chartMax = n ? 100 : initialCapacity;
+            if(n) {
+                _.each($scope.chart1.segments, function(segment){
+                    segment.value = (segment.value / initialCapacity * 100).toFixed(1) * 1;
+                });
+            } else {
+                _.each($scope.chart1.segments, function(segment, i){
+                    segment.value = initialSegments[i].value;
+                });
+            }
+        });
+
+        $scope.addSector = function(){
+            $scope.chart1.segments.push($scope.newSegment);
+            initialSegments = angular.copy($scope.chart1.segments);
+            $scope.newSegment = {};
         };
 
     }]);
